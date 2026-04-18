3988132b0049b52a6d5da891f0b31899fcf5b79c
redathana-lang <redathana@gmail.com>
2026-04-18 17:23:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-v1';
+const CACHE = 'flow-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
